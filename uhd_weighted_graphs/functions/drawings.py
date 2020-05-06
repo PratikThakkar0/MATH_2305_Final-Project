e858f1ec4876d706bf7c40c51c5b13049b4ee643
Pratik Thakkar <34815334+PratikThakkar0@users.noreply.github.com>
2020-05-05 23:31:20 -0500

    Update drawings.py

--- uhd_weighted_graphs/functions/drawings.py
+++ uhd_weighted_graphs/functions/drawings.py
@@ -2,7 +2,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 
-def show_weighted_graph(G):
+def show_weighted_graph(G):                                   # defines and displays the weighted graph
     pos = nx.planar_layout(G)
     nx.draw(G, pos)
     labels = nx.get_edge_attributes(G, 'weight')
@@ -12,18 +12,18 @@ def show_weighted_graph(G):
     plt.show()
    
 
-def draw_subtree(G, T):
+def draw_subtree(G, T):                                      # defines and displays the sub-graph
     pos = nx.planar_layout(G)
     nx.draw_networkx(G, pos)
     labels = nx.get_edge_attributes(G, 'weight')
-    nx.draw_networkx_edge_labels(G,
+    nx.draw_networkx_edge_labels(G,                          #displays the labels of edges of the Sub-Graph
                                  pos,
                                  edge_labels = labels,)
-    nx.draw_networkx_edges(G, pos,
+    nx.draw_networkx_edges(G, pos,                           #displays the edges of the Sub-Graph
                             edgelist=T.edges(),
                             width=8, alpha=0.5,
                             edge_color='r',)
-    nx.draw_networkx_nodes(G,
+    nx.draw_networkx_nodes(G,                               #displays the nodes of theSub- Graph
                            pos,
                            nodelist=T.nodes(),
                            node_color='r',
